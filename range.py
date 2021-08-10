ddc4571548bfd39b33636a667fe4d96891bdc52f
AugustRo <64295693+AugustRo@users.noreply.github.com>
2021-08-10 14:51:29 -0500

    Rangos

---
+++ range.py
@@ -0,0 +1,9 @@
+
+impar= range (1,100,2)
+par= range(0,101,2)
+print("Impares: \n")
+for i in impar:
+    print(f'{i}\t', end=' ')
+print("\n\nPares: \n")
+for i in par:
+    print(f'{i}\t', end=' ')
